3fcb391efc695c9ffacf5ec77681ccbd06306642
Imelda Atieno <imeldaatieno01@gmail.com>
2022-09-12 19:00:09 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Execute
+ *
+ * Return: always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+
+	printf("\n");
+	return (0);
+}
