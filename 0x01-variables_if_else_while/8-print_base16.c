674e3f8a36f77e45997433e45d7fb20be8957a8e
Imelda Atieno <imeldaatieno01@gmail.com>
2022-09-12 19:44:50 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Execute
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char a, i;
+
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(i);
+
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
