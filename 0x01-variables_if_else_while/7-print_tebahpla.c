8784e1157aee0487a88b2cc051570a6544bf17d7
Imelda Atieno <imeldaatieno01@gmail.com>
2022-09-12 19:18:22 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Execute
+ *
+ * Return: always 0 (Success)
+ *
+ * int main(void)
+ * {
+ * char i;
+ *
+ * for (i = 'z'; i >= 'a'; i--)
+ * putchar(i);
+ *
+ * putchar('\n');
+ * return (0);
+ * }
