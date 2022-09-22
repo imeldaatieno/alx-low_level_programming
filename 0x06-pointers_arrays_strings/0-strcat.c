40af7a652af73723efae73d3ec8871d97d105a80
Imelda Atieno <imeldaatieno01@gmail.com>
2022-09-22 07:27:54 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,11 +2,11 @@
 
 /**
 *_strcat - concatenates  the string pointed to by @src to
-* the end of the string pointed to by @dest
-*@dest: String that will be appended
-*@src: String to be concatenated upon
+* the end of the string will be pointed to by @dest
+*dest: String that will appended
+@src: String to be concatenated upon
 *
-* Return: returns poiner to @dest##
+* Return: returns pointer to @dest###
 
 char *_strcat(char *dest, char *src)
 {
@@ -17,7 +17,7 @@ while (dest[index++])
 dest_len++;
 
 for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
+destdest_len++] = src[index];
 
 return (dest);
 }
