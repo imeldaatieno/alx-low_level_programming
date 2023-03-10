13085820eec991a94ff1628722fd7bcf53287dac
Imelda Atieno <imeldaatieno01@gmail.com>
2023-03-10 00:19:06 -0800

    0.Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,26 @@
-#ifndef DOG_H_FILE
-#define DOG_H_FILE
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
- * struct dog - structure definition of a dog
- * @name: dog name
- * @age: dog age
- * @owner: owner
+ * struct dog - Dog attributes
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: The attributes of the dog.
  */
-tyedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t
+};
+
+/**
+ * dog_t- Typedef for dog structure
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 #endif
