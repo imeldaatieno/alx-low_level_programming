ecdbfa99aea7cb1b6f4eea03e66152ea3e266a88
Imelda Atieno <imeldaatieno01@gmail.com>
2023-07-12 16:36:00 -0700

    Changes to 0-whatsmyname file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - prints the name of the program
+* main - prints the name of the program followed by a new line
 * @argc: number of arguments passed the function
 * @argv: argument vector of pointers to string
+*
 * Return: always 0
-*/
-int main(int argc __attribute__((unused)), char *argv[])
+**/
+
+int main(int argc, char *argv[])
 {
-printf("%s\n", argv[0]);
-return (0);
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
 }
