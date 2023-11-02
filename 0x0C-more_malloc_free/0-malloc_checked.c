d02021ac4f7cca1f91eb4a78c36682209993ca0c
Imelda Atieno <imeldaatieno01@gmail.com>
2023-11-02 05:09:20 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+/**
+ * main - a function that allocates memory using malloc
+ *
+ * Return: pointer to the allocated memory.
+ */
+
+void *malloc_checked(unsigned int b);
+{
+	int b;
+
+	b = malloc_checked(sizeof(char) * 98);
+	_putchar("%p\n", (void *)b);
+	free(b);
+	return (0);
+}
