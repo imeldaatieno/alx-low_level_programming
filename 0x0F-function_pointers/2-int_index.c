3851d1ff70fa06f9a6c55c880db842f67b0e0ad5
Imelda Atieno <imeldaatieno01@gmail.com>
2024-01-17 04:10:12 -0800

    Updating this directory

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,7 +6,7 @@
  * @array: array
  * @size: size of the array
  * @cmp: points to the function used to compare values
- * Return: 0
+ * Return: Always 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
