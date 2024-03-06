e355a95504475086dd228bb8bf43ff736a8f9be5
Imelda Atieno <imeldaatieno01@gmail.com>
2024-03-06 03:27:46 -0800

    8. Hexadecimal Correction

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main -Execute
+ * main - Prints all the numbers of base 16 in lowercase, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  *
  */
+
 int main(void)
 {
-	char a, i,;
+char i, j;
 
-	for (i = '0'; i <= '9'; i++)
-		putchar(i);
+for (i = '0'; i <= '9'; i++)
+	putchar(i);
 
-	for (a = 'a'; a <= 'f'; a++)
-		putchar(a);
+for (j = 'A'; j <= 'F'; j++)
+	putchar(j);
 
-	putchar('\n');
-	return (0);
+putchar ('\n');
+
+return (0);
 }
-
