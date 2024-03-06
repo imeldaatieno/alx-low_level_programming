8ba5153c6edc39af5c8d8c0e82c568790a29dea5
Imelda Atieno <imeldaatieno01@gmail.com>
2024-03-06 03:12:16 -0800

    Corrections to 3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,10 @@ int main(void)
 {
 char alphabet;
 
-for (alphabet = "a"; alphabet <= "z"; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	putchar(alphabet);
 
-for (i = "A"; alphabet <= "Z"; alphabet++)
+for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 	putchar(alphabet);
 
 putchar(\n);
